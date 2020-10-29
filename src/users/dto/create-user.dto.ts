92797b692b104a7da62bde8081b91585830e96e1
Mosly <moritz.wenko@gmx.net>
2020-10-29 11:55:59 +0100

    dto validation user

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,7 +1,22 @@
+import { ArrayMaxSize, ArrayMinSize, IsString, Matches, MinLength } from 'class-validator';
+
 export class CreateUserDto {
+    @IsString()
     firstName: string;
+
+    @IsString()
+    @MinLength(10)
     lastName: string;
+
     fh: string;
+
+    @ArrayMaxSize(10)
+    @ArrayMinSize(1)
     languages: string[];
+
+    @Matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).{6,64}$/m, {
+        message:
+            'Password must be between 6 and 64 characters long with 1 special character and capital character each',
+    })
     password: string;
 }
